324a7e02a759bc14b502a91b25a9bc1806067274
Challenging <challenging666@qq.com>
2019-06-04 01:27:53 +0800

    完成正则文件加载

--- src/com/period/PeriodNormalizer.java
+++ src/com/period/PeriodNormalizer.java
@@ -4,9 +4,14 @@ import com.time.nlp.TimeNormalizer;
 import com.time.nlp.TimeUnit;
 import com.time.nlp.stringPreHandlingModule;
 
+import java.io.*;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @program: Time-NLP
@@ -19,9 +24,14 @@ public class PeriodNormalizer {
 
     private PeriodUnit[] periods;
     private TimeUnit[] times;
+    private static Pattern patterns = null;
 
     public PeriodNormalizer(){
-
+        try {
+            patterns = readModel();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 
@@ -47,7 +57,16 @@ public class PeriodNormalizer {
     * @description  抽取时间段
      */
     private void periodExtract(String maskStr){
-
+        Matcher matcher;
+        int startLine = -1, endLine = -1;
+        matcher = patterns.matcher(maskStr);
+        System.out.println("patterns: "+ patterns);
+        System.out.println("Str: "+ maskStr);
+        while (matcher.find()){
+            startLine = matcher.start();
+            System.out.println(matcher.group());
+            
+        }
     }
 
 
@@ -107,5 +126,82 @@ public class PeriodNormalizer {
         return maskStr;
     }
 
+    /**
+    * @author LinZheng Chai
+    * @date 2019/6/4 0:26
+    * @param
+    * @return
+    * @description 读取正则模型, 返回所有正则表达式的拼接
+     */
+    private Pattern readModel(){
+        String[] files = new String[] {
+                "/period1.txt",
+                "/period2.txt",
+        };
+        List<String> regexArr = new ArrayList<>();
+
+        for (String file : files){
+            regexArr.add(loadRegexFile(file));
+        }
+
+        String allRegex = String.join("|", regexArr);
+        return Pattern.compile(allRegex);
+    }
+
+    /**
+    * @author LinZheng Chai 
+    * @date 2019/6/4 0:53
+    * @param 
+    * @return 
+    * @description 加载正则表达式文件,并把每行用|连接起来
+     */
+    public String loadRegexFile(String path){
+        List<String> regexArr = new ArrayList<>();
+        try {
+            //编码格式
+            String encoding = "UTF-8";
+            //文件路径
+            InputStream in = getClass().getResourceAsStream(path);
+            //输入流
+            InputStreamReader read = new InputStreamReader(in, encoding);// 考虑到编码格
+            BufferedReader bufferedReader = new BufferedReader(read);
+            String lineTxt = null;
+            //读取一行
+            while ((lineTxt = bufferedReader.readLine()) != null) {
+                //正则表达式
+                System.out.println(lineTxt);
+                regexArr.add(lineTxt);
+
+            }
+            read.close();
+        } catch (Exception e) {
+            System.out.println("读取文件内容出错");
+            e.printStackTrace();
+        }
+        return String.join("|", regexArr);
+    }
+
+    public static void main(String[] args){
+        try {
+            //编码格式
+            String encoding = "UTF-8";
+            //文件路径
+
+            File file = new File("C:\\Users\\Challenging\\Desktop\\NLPLearning\\Time-NLP\\resource\\period1.txt");
+            //输入流
+            InputStreamReader read = new InputStreamReader(new FileInputStream(file), encoding);// 考虑到编码格
+            BufferedReader bufferedReader = new BufferedReader(read);
+            String lineTxt = null;
+            //读取一行
+            while ((lineTxt = bufferedReader.readLine()) != null) {
+                //正则表达式
+                System.out.println(lineTxt);
+            }
+            read.close();
+        } catch (Exception e) {
+            System.out.println("读取文件内容出错");
+            e.printStackTrace();
+        }
+    }
 
 }
